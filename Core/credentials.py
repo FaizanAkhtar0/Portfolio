bff278be34b2092191230baa01fe7ded736765c2
FaizanAkhtar0 <74153102+FaizanAkhtar0@users.noreply.github.com>
2021-07-07 18:10:07 +0500

    Delete credentials.py
    Final Release 3.0

--- Core/credentials.py
+++
@@ -1,6 +0,0 @@
-git_token = "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"
-acc_owner = "FaizanAkhtar0"
-query_url = f"https://api.github.com/users/{acc_owner}/repos"
-params = {"state": "open"}
-headers = {'Authorization': f'token {git_token}'}
-
